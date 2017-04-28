b690d55be08ab412b7b0cd673cbe012551cc2cbe
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2017-04-27 23:41:47 -0700

    Temporarily debug change
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- train.py
+++ train.py
@@ -204,10 +204,10 @@ print('Loading steer data')
 low_steer, high_steer = load_steer_data()
 random.shuffle(low_steer)
 random.shuffle(high_steer)
-low_steer_train = low_steer[:int(0.9*len(low_steer))]
-high_steer_train = high_steer[:int(0.9*len(high_steer))]
-low_steer_val = low_steer[int(0.9*len(low_steer)):]
-high_steer_val = high_steer[int(0.9*len(high_steer)):]
+low_steer_train = low_steer[:int(0.01*len(low_steer))]
+high_steer_train = high_steer[:int(0.01*len(high_steer))]
+low_steer_val = low_steer[int(0.01*len(low_steer)):]
+high_steer_val = high_steer[int(0.01*len(high_steer)):]
 
 net, criterion, optimizer = instantiate_net()  # TODO: Load neural net from file
 
